1f4a9dbb21b78ff2f3dd63130d57f2351a0ea568
Patainea Robert <patainearobert@yahoo.com>
2022-02-25 16:11:50 +0200

    display movies + pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import Movie from "./Movie";
+import Movie, { MovieProps } from "./Movie";
+
+type PopularMovies = {
+  page: number;
+  results: MovieProps[];
+};
 
 function App() {
-  return (
-    <div className="App">
-      <Movie id="550" />
-      <Movie id="100" />
-      <Movie id="200" />
-    </div>
+  const [page, setPage] = useState(1);
+  const [popularMovies, setPopularMovies] = useState<PopularMovies | null>(
+    null
   );
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=82c8eb95d42bd3158187fd36f6681c75&page=${page}`
+      )
+      .then((response) => {
+        setPopularMovies(response.data);
+      });
+  }, [page]);
+
+  const renderPopularMovies = () => {
+    if (popularMovies === null) return null;
+
+    return popularMovies.results.map((movie: MovieProps) => (
+      <Movie key={movie.id} id={movie.id} />
+    ));
+  };
+
+  const nextButton = () => {
+    return <button onClick = {() => {
+      setPage(page + 1)
+    }}>Next</button>
+  }
+
+  const previousButton = () => {
+    return <button onClick = {() => {
+      setPage(page - 1 > 0 ? page - 1 : page)
+    }}>Previous</button>
+  }
+
+  return <div className="App">
+    {renderPopularMovies()}
+    {previousButton()}
+    {nextButton()}
+  </div>;
 }
 
 export default App;
--- src/Movie.tsx
+++ src/Movie.tsx
@@ -1,48 +1,44 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-
-type MovieProps = {
+export type MovieProps = {
+  id: number;
   title: string;
   vote_average: number;
   release_date: string;
 };
 
 type Props = {
-    id: string;
-}
+  id: number;
+};
 
 export default function Movie(props: Props) {
+  const [movie, setMovie] = useState<MovieProps | null>(null);
 
-    const [movie, setMovie] = useState<MovieProps | null>(null);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${props.id}?api_key=82c8eb95d42bd3158187fd36f6681c75`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setMovie(response.data);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios
-          .get(
-            `https://api.themoviedb.org/3/movie/${props.id}?api_key=82c8eb95d42bd3158187fd36f6681c75`
-          )
-          .then((response) => {
-            console.log(response.data);
-            setMovie(response.data);
-          });
-      }, []);
-    
-      
-    
-      const renderMovie = () => {
-        if (movie === null) return;
-        return <div>
-          <div>
-            {movie.title}
-          </div>
-          <div>
-            Rating: {movie.vote_average}
-          </div>
-          <div>
-            Release Date: {movie.release_date}
-          </div>
-        </div>;
-      };
+  const renderMovie = () => {
+    if (movie === null) return;
+    return (
+      <div>
+        <div>
+          {movie.title} | Rating: {movie.vote_average} | Release Date:{" "}
+          {movie.release_date}
+        </div>
+
+        <hr />
+      </div>
+    );
+  };
 
   return <div>{renderMovie()}</div>;
 }
